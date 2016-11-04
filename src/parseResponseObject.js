3f5526e8a4a69ab126818572afd0407f5096b42d
Micah Stubbs <micah.stubbs@gmail.com>
2016-11-03 17:55:48 -0700

    lookup actual value for categorical columns from column domain sent by h2o-3 fix #3

--- src/parseResponseObject.js
+++ src/parseResponseObject.js
@@ -5,7 +5,16 @@ export function parseResponseObject(responseObject) {
     if (Object.prototype.toString.call(d.data) === '[object Array]') {
       d.data.forEach((e, j) => {
         if (typeof points[j] === 'undefined') points[j] = {};
-        points[j][d.label] = e;
+        if (Object.prototype.toString.call(d.domain) === '[object Array]') {
+          // if the domain is an array
+          // then this is a categorical column
+          // and we want to lookup the actual values from the domain
+          // using the domain indices stored in data
+          points[j][d.label] = d.domain[e];
+        } else {
+        // else this is a numeric column
+          points[j][d.label] = e;
+        }
       });
     }
   });
