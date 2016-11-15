97b63eb02b823e3c1d039476dba6ccc5df145480
Micah Stubbs <micah.stubbs@gmail.com>
2016-11-14 17:34:24 -0800

    abstract out getSubMembersFrameData

---
+++ src/getSubMemberFrameData.js
@@ -0,0 +1,22 @@
+export function getSubMemberFrameData(options, callback) {
+  const frame = options.frame;
+  const server = options.server;
+  const port = options.port;
+  const exemplarsFrameID = options.exemplarsFrameID;
+  const columnOffset = options.columnOffset;
+  const vis = options.vis;
+
+  // get the data for the sub-member frame
+  // specify columnCount and rowCount so that h2o-3 will return all data from the frame
+  //
+  const columnCount = frame.columnCount;
+  const rowCount = frame.rowCount;
+  const getFrameURL = `http://${server}:${port}/3/Frames/${exemplarsFrameID}?column_offset=${columnOffset}&column_count=${columnCount}&row_count=${rowCount}`;
+  console.log('getFrameURL', getFrameURL);
+  fetch(getFrameURL, { method: 'GET' })
+    .then(res => res.json())
+    .then(json => {
+      // pass the json data to the provided callback
+      callback(null, json, vis);
+    });
+}
